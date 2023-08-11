6fa3bd1916031ffe78b8603f186dceaa4b6b3fc7
Siteshdas <siteshdas01@gmail.com>
2023-08-11 11:31:01 +0530

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,138 @@
+import images from "./images";
+
+const services = [
+    {
+        image: `${images.services_img_1}`,
+        title: "Website designing",
+        paragraph: "I design and develop services for customers of all sizes, specializing in creating stylish, modern websites."
+    },
+    {
+        image: `${images.services_img_2}`,
+        title: "Branding",
+        paragraph: "Vivamus a accumsan mauris. Nunc sodales lacus nec felis suscipit dictum. Mauris auctor enim in justo euismod imperdiet."
+    },
+    {
+        image: `${images.services_img_3}`,
+        title: "Social media Handeling",
+        paragraph: "Vivamus a accumsan mauris. Nunc sodales lacus nec felis suscipit dictum. Mauris auctor enim in justo euismod imperdiet."
+    },
+    {
+        image: `${images.services_img_4}`,
+        title: "Logo Creating",
+        paragraph: "Vivamus a accumsan mauris. Nunc sodales lacus nec felis suscipit dictum. Mauris auctor enim in justo euismod imperdiet."
+    },
+    {
+        image: `${images.services_img_5}`,
+        title: "Youtube thumbnail",
+        paragraph: "Vivamus a accumsan mauris. Nunc sodales lacus nec felis suscipit dictum. Mauris auctor enim in justo euismod imperdiet."
+    },
+    {
+        image: `${images.services_img_6}`,
+        title: "Graphics Designing",
+        paragraph: "Vivamus a accumsan mauris. Nunc sodales lacus nec felis suscipit dictum. Mauris auctor enim in justo euismod imperdiet."
+    },
+];
+
+const works = [
+    {
+        image: `${images.work_img_1}`
+    },
+    {
+        image: `${images.work_img_2}`
+    },
+    {
+        image: `${images.work_img_3}`
+    },
+    {
+        image: `${images.work_img_4}`
+    },
+    {
+        image: `${images.work_img_5}`
+    },
+    {
+        image: `${images.work_img_6}`
+    },
+];
+
+const work_process = [
+    {
+        title: "Dental site",
+        paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        title: "Post Design",
+        paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        title: "Develop",
+        paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        title: "Cart-site",
+        paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+];
+
+const about_stats = [
+    {
+        image: `${images.stat_img_1}`,
+        value: "5",
+        title: "Clients"
+    },
+    {
+        image: `${images.stat_img_2}`,
+        value: "6",
+        title: "Projects"
+    },
+    {
+        image: `${images.stat_img_3}`,
+        value: "100",
+        title: "Working Hours"
+    },
+    {
+        image: `${images.stat_img_4}`,
+        value: "1",
+        title: "Awards"
+    },
+];
+
+const testimonials = [
+    {
+        name: "Dr. Abhishek Pati",
+        post: "Doctor",
+        paragraph: "i am so happy to my graphic design post ."
+    },
+    {
+        name: "Dr. Sauvik Singha",
+        post: "Doctor",
+        paragraph: "Well design website created by sitesh , thanks  "
+    },
+    {
+        name: "Mukesh Sing",
+        post: "Party Organiser",
+        paragraph: "Thanks for creating a good design website "
+    },
+]
+
+const logos = [
+    {
+        image: `${images.logo_img_1}`
+    },
+    {
+        image: `${images.logo_img_2}`
+    },
+    {
+        image: `${images.logo_img_3}`
+    },
+    {
+        image: `${images.logo_img_4}`
+    },
+    {
+        image: `${images.test_img_5}`
+    },
+    {
+        image: `${images.test_img_6}`
+    },
+];
+
+export {services, works, work_process, about_stats, testimonials, logos};
